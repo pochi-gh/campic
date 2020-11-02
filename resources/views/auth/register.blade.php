cc39988df693c12fb24096ec00361cca9100908c
pochi-gh <pochigm@gmail.com>
2020-11-02 16:05:38 +0900

    バリデーションの日本語化

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -26,10 +26,8 @@
                                 @enderror
                             </div>
                         </div>
-
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
@@ -40,7 +38,6 @@
                                 @enderror
                             </div>
                         </div>
-
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 

ba8891aa8d95861b6c83575a6d01074351d101a4
pochi-gh <pochigm@gmail.com>
2020-11-02 13:30:57 +0900

    リレーションの追加

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -3,43 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Article;
 
 class ArticleController extends Controller
 {
   public function index()
   {
-    $articles = [
-      (object) [
-        'id'         => 1,
-        'title'      => 'タイトル1',
-        'image'      => 'https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg',
-        'created_at' => now(),
-        'user'       => (object) [
-          'id'       => 1,
-          'name'     => 'ユーザー名1',
-        ],
-      ],
-      (object) [
-        'id'         => 2,
-        'title'      => 'タイトル2',
-        'image'      => 'https://pbs.twimg.com/media/Elcidu5U8AAU8-s?format=jpg&name=large',
-        'created_at' => now(),
-        'user'       => (object) [
-          'id'       => 2,
-          'name'     => 'ユーザー名2',
-        ],
-      ],
-      (object) [
-        'id'         => 3,
-        'title'      => 'タイトル3',
-        'image'      => 'https://pbs.twimg.com/media/ElaSeb-U0AELb97?format=jpg&name=large',
-        'created_at' => now(),
-        'user'       => (object) [
-          'id'       => 3,
-          'name'     => 'ユーザー名3',
-        ],
-      ],
-    ];
+    $articles = Article::all()->sortByDesc('created_at');
+    
     return view('articles.index', compact('articles'));
   }
 }

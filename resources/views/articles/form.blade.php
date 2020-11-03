e6765a1f40a7bb4106ef2c8193eab8cd12929e8c
pochi-gh <pochigm@gmail.com>
2020-11-03 21:37:33 +0900

    編集周りの実装

--- resources/views/articles/form.blade.php
+++ resources/views/articles/form.blade.php
@@ -1,9 +1,9 @@
 @csrf
 <div class="md-form">
   <label>タイトル</label>
-  <input type="text" name="title" class="form-control" required value="{{ old('title') }}">
+  <input type="text" name="title" class="form-control" required value="{{ $article->title ?? old('title') }}">
 </div>
 <div class="form-group">
   <label></label>
-  <textarea name="image" required class="form-control" rows="2" placeholder="画像のURL">{{ old('image') }}</textarea>
+  <textarea name="image" required class="form-control" rows="2" placeholder="画像のURL">{{ $article->image ?? old('image') }}</textarea>
 </div>
--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -17,19 +17,67 @@
         <!-- Card content -->
         <div class="card-body card-body-cascade text-center">
           <!-- Title -->
-          <h4 class="card-title"><strong>
-            {{$article->title}}
-          </strong></h4>
-          <!-- Subtitle -->
-
-          <!-- Text -->
-          <p class="card-text">
-            本文
-          </p>
-          <div class="font-weight-bold indigo-text py-2">
-            {{$article->user->name}}
-            {{ $article->created_at->format('Y/m/d H:i') }} {{--この行を変更--}}
+          <div class="d-flex flex-row">
+            <h4 class="card-title"><strong>
+              {{$article->title}}
+            </strong></h4>
+        @if( Auth::id() === $article->user_id )
+        <!-- dropdown -->
+        <div class="ml-auto card-text">
+          <div class="dropdown">
+            <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <button type="button" class="btn btn-link text-muted m-0 p-2">
+                <i class="fas fa-ellipsis-v"></i>
+              </button>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right">
+              <a class="dropdown-item" href="{{ route("articles.edit", ['article' => $article]) }}">
+                <i class="fas fa-pen mr-1"></i>内容の更新
+              </a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item text-danger" data-toggle="modal" data-target="#modal-delete-{{ $article->id }}">
+                <i class="fas fa-trash-alt mr-1"></i>内容の削除
+              </a>
+            </div>
           </div>
+        </div>
+        <!-- dropdown -->
+
+        <!-- modal -->
+        <div id="modal-delete-{{ $article->id }}" class="modal fade" tabindex="-1" role="dialog">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <form method="POST" action="{{ route('articles.destroy', ['article' => $article]) }}">
+                @csrf
+                @method('DELETE')
+                <div class="modal-body">
+                  {{ $article->title }}を削除します。よろしいですか？
+                </div>
+                <div class="modal-footer justify-content-between">
+                  <a class="btn btn-outline-grey" data-dismiss="modal">キャンセル</a>
+                  <button type="submit" class="btn btn-danger">削除する</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <!-- modal -->
+      @endif
+        </div>
+        <!-- Text -->
+        <p class="card-text">
+        本文
+        </p>
+        <div class="font-weight-bold indigo-text py-2">
+        {{$article->user->name}}
+        {{ $article->created_at->format('Y/m/d H:i') }} {{--この行を変更--}}
+        </div>
+
         </div>
       </div>
     @endforeach

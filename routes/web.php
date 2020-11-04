3e3b094b7414ccd88f1f73b42e099216de675353
pochi-gh <pochigm@gmail.com>
2020-11-04 09:49:56 +0900

    詳細ページの実装

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@
 use App\Http\Controllers\ArticleController;
 
 Route::get('/','ArticleController@index')->name('articles.index');
-Route::resource('/articles','ArticleController')->except(['index'])->middleware('auth');
+Route::resource('/articles','ArticleController')->except(['index','show'])->middleware('auth');
+Route::resource('/articles','ArticleController')->only(['show']);
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
